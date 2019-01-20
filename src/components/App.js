12df4e500c48800851ddeb177ee50e7784ede801
MarloMorgan <granatogi@gmail.com>
2019-01-20 17:23:37 +0000

    react from scratch and redux

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AddToDo from '../containers/AddToDo';
+import VisibleTodoList from '../containers/VisibleTodoList'
+
+const App = () => (
+  <div>
+    <AddToDo />
+    <VisibleTodoList />
+  </div>
+)
+export default App
